50cb298ceb5fd081f981295bb0a90bfcae5c3a2a
Vrain Ahuja <vrainuncensored@gmail.com>
2020-03-15 21:49:01 -0700

    Adding basic HTMl website and  have 2 functions with alpaca

--- trade.py
+++ trade.py
@@ -1,9 +1,24 @@
-import requests
+import requests, json
 #This is the library we import to successfully make endpoint requests
 # we entered into terminal=  "pip3 instal requests"
-
+from config import *
+#This is getting all the files from our config file!
 BASE_URL = "https://api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+# the Alpaca documentation requires us include headers which is why we are adding these
 
-r = requests.get(BASE_URL) 
+#THIS THE METHOD FOR MAKING ADDONS TO A BASE URL FOR MAKING A CALL
 
-print(r.content)
+def get_account():
+    r = requests.get(ACCOUNT_URL, headers= HEADERS )
+    #THIS THE METHOD FOR MAKING ADDONS TO A BASE URL FOR MAKING A CALL
+    json_format1 = json.loads(r.text)
+    print(json_format1['cash'])
+
+def create_order():
+    r = requests.get(ACCOUNT_URL, headers= HEADERS )
+    json_format2 = json.loads(r.text)
+    print(json_format2)
+    
