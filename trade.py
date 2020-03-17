639c29f0adfb25a8dcd41b888b7cd716d51d0587
Vrain Ahuja <vrainuncensored@gmail.com>
2020-03-17 12:41:23 -0700

    finalized the changes to implement the alpaca api

--- trade.py
+++ trade.py
@@ -17,7 +17,24 @@ def get_account():
     json_format1 = json.loads(r.text)
     print(json_format1['cash'])
 
-def create_order():
-    r = requests.get(ACCOUNT_URL, headers= HEADERS )
-    json_format2 = json.loads(r.text)
-    print(json_format2)
+def create_order(symbol, qty, side, type, time_in_force):
+    data =  {
+        "symbol" : symbol,
+        "qty" : qty,
+        "side" : side,
+        "type" : type,
+        "time_in_force" : time_in_force
+    }
+    r = requests.post(ORDERS_URL, json=data,  headers= HEADERS )
+    return json.loads(r.content)
+
+def get_order():
+    r = requests.get(ORDERS_URL, headers= HEADERS )
+    return json.loads(r.content)
+
+#response = create_order("AAPL", 100, "buy", "market", "gtc" )
+
+#print(response)
+order = get_order()
+
+print(order)
